e159374fd5f2e96916ce52b0b5232eaa302e55fe
I-am-Lokesh <202052324@iiitvadodara.ac.in>
2023-09-09 17:59:15 +0530

    Added fontend URL to Access-control-allow-origin

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config({
 export const app = express () ;
 app.use(express.json()) ;
 app.use(express.urlencoded({extended:true})) ;
-app.use(cookieParser())
+app.use(cookieParser()) ; 
 
 app.use(cors({
   origin: [process.env.FRONTEND_URL],
