22458f8a763210ee85ca1b8a2728fd9cb3f4e038
I-am-Lokesh <202052324@iiitvadodara.ac.in>
2023-09-10 11:51:49 +0530

    CORS issue

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-import express from "express" 
-import userRouter from "./routes/users.js"
-import destRouter from "./routes/destinations.js"
-import ticketRouter from "./routes/tickets.js"
+import express from "express";
+import userRouter from "./routes/users.js";
+import destRouter from "./routes/destinations.js";
+import ticketRouter from "./routes/tickets.js";
 import cookieParser from "cookie-parser";
-import {config} from "dotenv"
-import cors from "cors" 
-import paymentRouter from "./routes/payment.js"
+import { config } from "dotenv";
+import cors from "cors";
+import paymentRouter from "./routes/payment.js";
 
 config({
-  path:"./config.env"
+  path: "./config.env",
+});
+
+export const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
+);
 
-export const app = express () ;
-app.use(express.json()) ;
-app.use(express.urlencoded({extended:true})) ;
-app.use(cookieParser()) ; 
-
-app.use(cors({
-  origin: [process.env.FRONTEND_URL],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}))
- 
-
-app.use("/api/v1/user",userRouter) ;
-app.use("/api/v1/destination",destRouter) ;
-app.use("/api/v1/tickets",ticketRouter) ;
-app.use("/api/v1/payment",paymentRouter) ;
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/destination", destRouter);
+app.use("/api/v1/tickets", ticketRouter);
+app.use("/api/v1/payment", paymentRouter);
 
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    message : "Welcome to Travler Backend",
+    message: "Welcome to Travler Backend",
   });
 });
-
